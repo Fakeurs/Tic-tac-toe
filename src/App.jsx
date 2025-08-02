cc359e682d2d6f14a83cbdd05ac83a390b914170
Ridwan <ridghamy@gmail.com>
2025-08-02 22:24:43 +0400

    Added labels for winner and next player's turn

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,22 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [disables, setDisable] = useState(Array(9).fill(false));
+  let status;
+
+  const winner = calculateWinner(squares);
+
+  if (winner) {
+    status = `Winner is: ${winner}`;
+    console.log(`Winner is: ${winner}`);
+  } else {
+    status = `Next player is : ${xIsNext ? "X" : "O"}`;
+  }
 
   function handleClick(i) {
+    if (calculateWinner(squares)) {
+      return;
+    }
+
     const nextSquares = [...squares];
     const remainingDisables = [...disables];
     if (xIsNext && disables[i] === false) {
@@ -36,6 +50,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
       <BoardRow>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -53,4 +68,24 @@ export default function Board() {
       </BoardRow>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]; // either X or O win
+    }
+  }
+  return null; // match is a tie
 }
